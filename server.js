60c0b9cc44de3eb50887cdb499e8d0a2327397ec
Dhruv Rishishwar <63316728+Sneakyhydra@users.noreply.github.com>
2022-06-13 14:46:47 +0530

    New commit

---
+++ server.js
@@ -0,0 +1,54 @@
+/**
+ * Imports
+ */
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const compression = require('compression');
+
+/**
+ * Database
+ */
+const connectDB = require('./config/db');
+connectDB();
+
+/**
+ * Set Port
+ */
+const port = process.env.PORT || 5000;
+
+/**
+ * Initialize Express
+ */
+const app = express();
+
+/**
+ * Middlewares
+ */
+app.use(express.json({ extended: false }));
+app.use(cors());
+app.use(cookieParser());
+app.use(compression({ level: 5 }));
+
+/**
+ * Routes
+ */
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/users', require('./routes/users'));
+
+/**
+ * Serve static assets in production
+ */
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+}
+
+/**
+ * Listen to port
+ */
+app.listen(port, () => console.log(`Listening on port ${port}`));
